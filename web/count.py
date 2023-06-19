2ad2ce3454b8eb9defb87c2b061e6208cc184ef1
sniverty <sniverty@nvidia.com>
2023-06-19 14:18:33 -0700

    Add Heroku deployment files

--- web/count.py
+++ web/count.py
@@ -1,8 +1,10 @@
+import re, os, sys, time
 from flask import Flask, request, render_template
 import requests
-import re, os, sys
 import redis
 from rq import Worker, Queue, Connection
+from rq.job import Job
+from rq.exceptions import NoSuchJobError
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -30,7 +32,23 @@ def index():
         # count = count_words_in_url(url)
         job = q.enqueue('web.count.count_words_in_url', url)
         count = job.result
-    
+        while True:
+            try:
+                # Re-fetch the job to get updated status and result
+                job.refresh()
+                
+                if job.is_finished:
+                    # If the job is finished, its result is ready and can be used
+                    count = job.result
+                    break
+                else:
+                    # If the job is not finished, wait for a bit before checking again
+                    time.sleep(1)
+            except NoSuchJobError:
+                # The job does not exist, possibly because it's already finished and its data was expired
+                break  
+        print('count = ', count)
+        
     return render_template('index.html', count=count)
 
 if __name__ == '__main__':
